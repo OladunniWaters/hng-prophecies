70b7c63534bf1f80c460c595f4930dbecdf29498
Ijeoma Uwabunkeonye <uwabunkeonyeijeoma@gmail.com>
2022-11-25 16:07:11 +0100

    Feat:ProphecyData for Prophecy Website

---
+++ src/PropheciesData/AugData.js
@@ -0,0 +1,52 @@
+
+const AugData = [
+  {
+    id:57,
+    name:"Dr. Paul Enenche",
+    prophecy:"It is a season of increased authority for the child of God; a season of increased authority and royalty for the church; there shall be an increase in level of command; the realm of saying it and seeing it! Not just the pastor but the church",
+    image:"https://dailypost.ng/wp-content/uploads/2019/04/Pastor_Paul_Enenche.jpg.webp",
+  },
+    {
+    id:58,
+    name:"Prophet (Dr.) MKO Tibetan ",
+    prophecy:"There is going to be a terrible kidnapping next year, alot of prominent people will leave the State",
+image:"https://www.google.com/search?client=tablet-android-samsung-nf-rev1&sxsrf=ALiCzsZyFK4K-fMq-bm3u4h4bu7xSndOyA:1669320214368&q=Prophet+Tibetan+prophecy+for+2022&tbm=isch&sa=X&ved=2ahUKEwiC35Kzzsf7AhWiIMUKHQftC4IQ0pQJegQIChAB&biw=1503&bih=902&dpr=1.33#imgrc=4yLHAN37hxN_yM",
+},
+    {
+    id:59,
+    name:"Pastor Adebayo",
+    prophecy:"There will be two monstrous storms (one coming from the Atlantic and one from the Pacific) unless they are weakened, the results will be terrible"
+image:"https://upload.wikimedia.org/wikipedia/commons/e/e8/Eaa1.jpg",
+  },
+     {
+    id:60,
+    pastor:"Bishop Oyedepo",
+    prophecy:"All your mockers will be alive to see the glory of God in your life. This year shall be the most robust spiritual year in your life. Prophetic Words will come to pass in your life without struggles. The Lord shall make a difference between you and the people of the world",
+   image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_KeSs2sYi_lDCp0lrLp539O7Y8uQw0SWJtA&usqp=CAU",
+  },
+      {
+    id:61,
+    pastor:"Pastor Samuel Ikechukwu",
+    prophecy: "Youths shall preach the gospel this year with signs following excessively make sure you are word worship and prayer addict; success is sure for anyone that lays hands daily on business or other clean work not the lazy Christian; soul-winning and giving in love opens your heaven and earth to yield increase to you 2022 .",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2bAGcbrxolMeGu6coQSoGMoDTn4RAZQ4CzQ&usqp=CAU",
+  },
+      {
+    id:62,
+    pastor:"Apostle Omotosho Tope",
+    prophecy: "Many will experience fruitfulness this year and have their babies; for those who lost job last year, get ready for mega jobs.",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIMPriRCbIZx1U0gX5c1XIJyOL45cDKCcYtw&usqp=CAU",
+  },
+    {
+    id:63,
+    pastor:"Prophet Nigel Gaisie",
+    prophecy: "Nigeria’s Vice President Yemi Osinbajo will become president.",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1F7N7HPSlqSCmPRPmFs-xGxSrBgGpEyH3Lw&usqp=CAUg",
+  },
+    {
+    id:64,
+    pastor:"Rev. Father. Mbaka",
+    prophecy: "The Holy Spirit of God asked me to warn the government of the day to be very careful; to change policies and take care of the youths without politicising their (youths’) condition.",
+    image:"https://believersportal.com/wp-content/uploads/2016/09/Rev.-Fr.-Ejike-Mbaka.jpg",
+  },
+  ]
+  export default AugData
